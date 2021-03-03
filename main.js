8dcc38e029ba8ed480193406afc8e4419086a885
Jagatperumal <74633014+Jagatperumal@users.noreply.github.com>
2021-03-02 18:28:46 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+console.log("My first console screen");
+var first_name="Jagat";
+console.log(first_name);
+var last_name=" Perumal";
+console.log(last_name);
+var full_name=first_name.concat(last_name);
